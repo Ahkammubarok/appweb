dfbb03af3ae4696cf4c755398609851442b0344d
Mahdalena <126873643+Ahkammubarok@users.noreply.github.com>
2025-05-07 13:40:18 +0700

    Update footer.php

--- admin/layout/footer.php
+++ admin/layout/footer.php
@@ -16,50 +16,50 @@
 
 <!-- jQuery -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script src="/cmp/admin/assets/plugins/jquery/jquery.min.js"></script>
+<script src="/appweb/admin/assets/plugins/jquery/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="/cmp/admin/assets/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="/appweb/admin/assets/plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
     $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="/cmp/admin/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/appweb/admin/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- ChartJS -->
-<script src="/cmp/admin/assets/plugins/chart.js/Chart.min.js"></script>
+<script src="/appweb/admin/assets/plugins/chart.js/Chart.min.js"></script>
 <!-- Sparkline -->
-<script src="/cmp/admin/assets/plugins/sparklines/sparkline.js"></script>
+<script src="/appweb/admin/assets/plugins/sparklines/sparkline.js"></script>
 <!-- JQVMap -->
-<script src="/cmp/admin/assets/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="/cmp/admin/assets/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="/appweb/admin/assets/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="/appweb/admin/assets/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="/cmp/admin/assets/plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="/appweb/admin/assets/plugins/jquery-knob/jquery.knob.min.js"></script>
 <!-- daterangepicker -->
-<script src="/cmp/admin/assets/plugins/moment/moment.min.js"></script>
-<script src="/cmp/admin/assets/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="/appweb/admin/assets/plugins/moment/moment.min.js"></script>
+<script src="/appweb/admin/assets/plugins/daterangepicker/daterangepicker.js"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="/cmp/admin/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="/appweb/admin/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
 <!-- Summernote -->
-<script src="/cmp/admin/assets/plugins/summernote/summernote-bs4.min.js"></script>
+<script src="/appweb/admin/assets/plugins/summernote/summernote-bs4.min.js"></script>
 <!-- overlayScrollbars -->
-<script src="/cmp/admin/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="/appweb/admin/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
-<script src="/cmp/admin/assets/dist/js/adminlte.js"></script>
+<script src="/appweb/admin/assets/dist/js/adminlte.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="/cmp/admin/assets/dist/js/pages/dashboard.js"></script>
+<script src="/appweb/admin/assets/dist/js/pages/dashboard.js"></script>
 <!-- DataTables  & Plugins -->
-<script src="/cmp/admin/assets/plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="/cmp/admin/assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="/cmp/admin/assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="/cmp/admin/assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="/cmp/admin/assets/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="/cmp/admin/assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="/cmp/admin/assets/plugins/jszip/jszip.min.js"></script>
-<script src="/cmp/admin/assets/plugins/pdfmake/pdfmake.min.js"></script>
-<script src="/cmp/admin/assets/plugins/pdfmake/vfs_fonts.js"></script>
-<script src="/cmp/admin/assets/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="/cmp/admin/assets/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="/cmp/admin/assets/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<script src="/appweb/admin/assets/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="/appweb/admin/assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="/appweb/admin/assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="/appweb/admin/assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="/appweb/admin/assets/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="/appweb/admin/assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="/appweb/admin/assets/plugins/jszip/jszip.min.js"></script>
+<script src="/appweb/admin/assets/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="/appweb/admin/assets/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="/appweb/admin/assets/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="/appweb/admin/assets/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="/appweb/admin/assets/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 
 <!-- datatable client side -->
 <script>
@@ -75,7 +75,7 @@
     processing: true,
     serverSide: true,
     ajax: {
-        url: "http://localhost/cmp/admin/_mahasiswa/mahasiswa-serverside.php?action=table_data",
+        url: "http://18.142.48.226/appweb/admin/_mahasiswa/mahasiswa-serverside.php?action=table_data",
         type: "POST",
         contentType: "application/x-www-form-urlencoded", // Pastikan format dikirim dengan benar
         data: { action: "fetch" }, // Kirim parameter action dengan benar
@@ -144,7 +144,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                <a href="/cmp/admin/logout.php" class="btn btn-danger">Logout</a>
+                <a href="/appweb/admin/logout.php" class="btn btn-danger">Logout</a>
             </div>
         </div>
     </div>
