4d61ca108240df5a0f5e8094c45d6d10adcbfecc
Venentian <ahkam.mahdalena.com>
2025-05-08 16:18:15 +0700

    hapus captha

--- admin/login.php
+++ admin/login.php
@@ -8,38 +8,23 @@ if (isset($_POST['login'])) {
     $username = mysqli_real_escape_string($db, $_POST['username']);
     $password = mysqli_real_escape_string($db, $_POST['password']);
 
-    // reCAPTCHA (test key dari Google)
-    //$secret_key = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe";
-    //$response_key = $_POST['g-recaptcha-response'] ?? '';
-
-    if (!empty($response_key)) {
-        $verify = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret_key&response=$response_key");
-        $response = json_decode($verify);
-
-        if ($response->success) {
-            $result = mysqli_query($db, "SELECT * FROM akun WHERE username = '$username'");
-            if (mysqli_num_rows($result) == 1) {
-                $hasil = mysqli_fetch_assoc($result);
-                if (password_verify($password, $hasil['password'])) {
-                    $_SESSION['login']      = true;
-                    $_SESSION['id_akun']    = $hasil['id_akun'];
-                    $_SESSION['nama']       = $hasil['nama'];
-                    $_SESSION['username']   = $hasil['username'];
-                    $_SESSION['email']      = $hasil['email'];
-                    $_SESSION['level']      = $hasil['level'];
-                    header("Location: index.php?login=success");
-                    exit;
-                } else {
-                    $error = true;
-                }
-            } else {
-                $error = true;
-            }
+    $result = mysqli_query($db, "SELECT * FROM akun WHERE username = '$username'");
+    if (mysqli_num_rows($result) == 1) {
+        $hasil = mysqli_fetch_assoc($result);
+        if (password_verify($password, $hasil['password'])) {
+            $_SESSION['login']      = true;
+            $_SESSION['id_akun']    = $hasil['id_akun'];
+            $_SESSION['nama']       = $hasil['nama'];
+            $_SESSION['username']   = $hasil['username'];
+            $_SESSION['email']      = $hasil['email'];
+            $_SESSION['level']      = $hasil['level'];
+            header("Location: index.php?login=success");
+            exit;
         } else {
-            $errorRecaptcha = true;
+            $error = true;
         }
     } else {
-        $errorRecaptcha = true;
+        $error = true;
     }
 }
 ?>
@@ -53,19 +38,6 @@ if (isset($_POST['login'])) {
     <title>Sign In</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="icon" href="assets/icon/login.png">
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            user-select: none;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-    </style>
     <link href="assets/css/signin.css" rel="stylesheet">
 </head>
 
@@ -81,12 +53,6 @@ if (isset($_POST['login'])) {
                 </div>
             <?php endif; ?>
 
-            <?php if (isset($errorRecaptcha)) : ?>
-                <div class="alert alert-danger text-center">
-                    <b>Recaptcha tidak valid</b>
-                </div>
-            <?php endif; ?>
-
             <div class="form-floating">
                 <input type="text" name="username" class="form-control" id="floatingInput" placeholder="Username" required>
                 <label for="floatingInput">Username</label>
@@ -95,15 +61,11 @@ if (isset($_POST['login'])) {
                 <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password" required>
                 <label for="floatingPassword">Password</label>
             </div>
-            <div class="mb-3 mt-3">
-                <!-- <div class="g-recaptcha" data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"></div> -->
-            </div>
-            <button class="w-100 btn btn-lg btn-primary" type="submit" name="login">Sign in</button>
+
+            <button class="w-100 btn btn-lg btn-primary mt-3" type="submit" name="login">Sign in</button>
             <p class="mt-5 mb-3 text-muted">Copyright &copy; FM Tech <?= date('Y') ?></p>
         </form>
     </main>
-
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </body>
 
 </html>
